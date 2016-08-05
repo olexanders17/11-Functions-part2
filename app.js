2d81da3c42fd8fd8dbb4483d5dfa7398f20461b6
olexanders <aaa@ss.ty>
2016-08-05 10:00:26 +0000

    add timer function

--- app.js
+++ app.js
@@ -4,10 +4,40 @@ function myTimer(ms) {
 
     while (true) {
         var nowDate = new Date().getTime();
-        if (( nowDate - startDate) >= 20000) {
-            console.log("munulo 3 sec" + new Date(nowDate - startDate).getSeconds());
+        if (( nowDate - startDate) >= limit) {
+            console.log("munulo  sec: " + new Date(nowDate - startDate).getSeconds());
             break;
         }
 
     }
 }
+
+var testFunction = function () {
+    console.log('I am test function');
+}
+
+var wrapper=function (func,ms) {
+    setTimeout(func,0)
+    func();
+};
+
+function setIntervalCustom(func, ms, maxCount) {
+    var count = 0;
+    console.log('run setIntervalCustom delay=' + ms + "iterations" + maxCount  );
+    while (true) {
+        myTimer(ms);
+        setTimeout(wrapper(func,0),0);
+
+        if (maxCount <= count) {
+            break
+        }
+        count++
+    }
+
+};
+
+
+console.log('Answer 1');
+setIntervalCustom(testFunction,3000,5);
+
+
